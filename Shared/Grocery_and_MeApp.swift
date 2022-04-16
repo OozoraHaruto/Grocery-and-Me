e2b3fb80510dadf87ffef29a0bf5f8e0ba55e8f3
春音(haruto) <chibi.taiyoozora@gmail.com>
2022-04-16 15:18:03 +0800

    Initial Commit

---
+++ Shared/Grocery_and_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_and_MeApp.swift
+//  Shared
+//
+//  Created by 春音 on 16/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_and_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
